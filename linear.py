7379e06a2f9c768245e71a3c38b784fb6feb2640
Vineet Kosaraju <vineetsk1@gmail.com>
2019-03-13 17:26:16 -0700

    linear algebra bugfixes

--- linear.py
+++ linear.py
@@ -35,33 +35,33 @@ class LinearUCB(Model):
         alpha = self.args.alpha
 
         # Assign a differerent weight/bias (A, b) for each arm
-        A1, A2, A3 = np.identity(d), np.identity(d), np.identity(d)
-        b1, b2, b3 = np.zeros(d), np.zeros(d), np.zeros(d)
+        A1, A2, A3 = np.identity(d), np.identity(d), np.identity(d) # Shapes (d, d)
+        b1, b2, b3 = np.zeros((d, 1)), np.zeros((d, 1)), np.zeros((d, 1)) # Shapes (d, 1)
 
         preds = []
 
         for t in range(T):
-            xt = features[t]
+            xt = features[t].reshape((d, 1)) # Shapes (d, 1)
             
-            ia1, ia2, ia3 = np.linalg.inv(A1), np.linalg.inv(A2), np.linalg.inv(A3)
+            ia1, ia2, ia3 = np.linalg.inv(A1), np.linalg.inv(A2), np.linalg.inv(A3) # Shapes (d, d)
 
-            tt1, tt2, tt3 = ia1.dot(b1), ia2.dot(b2), ia3.dot(b3)
-            pt1 = tt1.T.dot(xt) + alpha*np.sqrt(xt.T.dot(ia1).dot(xt))
-            pt2 = tt2.T.dot(xt) + alpha*np.sqrt(xt.T.dot(ia2).dot(xt))
-            pt3 = tt3.T.dot(xt) + alpha*np.sqrt(xt.T.dot(ia3).dot(xt))
+            tt1, tt2, tt3 = ia1.dot(b1), ia2.dot(b2), ia3.dot(b3) # Shapes (d, 1)
+            pt1 = tt1.T.dot(xt) + alpha*np.sqrt(xt.T.dot(ia1).dot(xt)) # Shapes (1, 1)
+            pt2 = tt2.T.dot(xt) + alpha*np.sqrt(xt.T.dot(ia2).dot(xt)) # Shapes (1, 1)
+            pt3 = tt3.T.dot(xt) + alpha*np.sqrt(xt.T.dot(ia3).dot(xt)) # Shapes (1, 1)
 
             pred = np.argmax([pt1, pt2, pt3]) + 1 # Arm: 1, 2, or 3
             r_t = 0 if pred == labels[t] else -1
 
             if pred == 1:
-                A1 = A1 + xt.T.dot(xt)
-                b1 = b1 + xt.dot(r_t)
+                A1 = A1 + xt.dot(xt.T) # Shapes (d, d)
+                b1 = b1 + r_t * xt     # Shapes (d, 1)
             elif pred == 2:
-                A2 = A2 + xt.T.dot(xt)
-                b2 = b2 + xt.dot(r_t)
+                A2 = A2 + xt.dot(xt.T) # Shapes (d, d)
+                b2 = b2 + r_t * xt     # Shapes (d, 1)
             elif pred == 3:
-                A3 = A3 + xt.T.dot(xt)
-                b3 = b3 + xt.dot(r_t)
+                A3 = A3 + xt.dot(xt.T) # Shapes (d, d)
+                b3 = b3 + r_t * xt     # Shapes (d, 1)
 
             preds.append(pred)
         
